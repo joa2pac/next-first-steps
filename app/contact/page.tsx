4d4d593bf62ac76a846a17b3fe9d2726d9856b90
joaquin <joaquinahuel93@gmail.com>
2024-07-23 22:56:31 -0300

    add metadata

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Page",
+  description: "Esta es la pagina de contacto de la empresa",
+};
+
 export default function ContacPage() {
   return (
     <>
--- app/princing/page.tsx
+++ app/princing/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Princing Page",
+  description: "Esta en la pagina de precios de mi servicio",
+};
+
 export default function PrincingPage() {
   return (
     <>
