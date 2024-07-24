7fedd5ef5fe76d32f04b74a38922283060266f1b
joaquin <joaquinahuel93@gmail.com>
2024-07-24 18:30:24 -0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span className="text-5xl">Hola Mundo!</span>
+      <Link href={"/general/about"}>About Page</Link>
     </main>
   );
 }
